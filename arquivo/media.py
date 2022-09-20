ff8cdaf4dd4a0fe3d80a62e1e389a65500a1a943
Claudio <c.diasbeatriz@gmail.com>
2022-09-20 01:17:19 -0300

    primeira versão

---
+++ arquivo/media.py
@@ -0,0 +1,9 @@
+Nota1 = float(input("digite a primeira nota do aluno:"))
+Nota2 = float(input("digite a segunda nota do aluno:"))
+
+media = (Nota1 + Nota2)/2
+
+if media >=7:
+    print("o aluno foi aprovado")
+else:
+    print("o aluno foi reprovado")
